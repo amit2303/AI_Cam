0d4399305c7da703cf65941cbc642e41e6978a7f
Amit Mishra <amitmishra0323@gmail.com>
2023-11-21 11:25:41 +0530

    app working on Physical Device

--- lib/main.dart
+++ lib/main.dart
@@ -1,87 +1,152 @@
 import 'dart:io';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_mlkit_image_labeling/google_mlkit_image_labeling.dart';
+
 import 'package:image_picker/image_picker.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Camera',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Camera'),
-    );
-  }
+  runApp(MyHomePage());
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-
-  final String title;
-
+  MyHomePage({Key? key}) : super(key: key);
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final ImagePicker picker = ImagePicker();
-
-
+  late ImagePicker imagePicker;
   File? _image;
-  chooseImage() async {
-    final XFile? image = await picker.pickImage(source: ImageSource.gallery);
-    if(image !=null){
+  String result = '';
+
+  //TODO declare ImageLabeler
+  dynamic imageLabeler;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    imagePicker = ImagePicker();
+    //TODO initialize labeler
+    final ImageLabelerOptions options =
+    ImageLabelerOptions(confidenceThreshold: 0.5);
+    imageLabeler = ImageLabeler(options: options);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    imageLabeler.close();
+  }
+
+  //TODO capture image using camera
+  _imgFromCamera() async {
+    XFile? pickedFile = await imagePicker.pickImage(source: ImageSource.camera);
+    _image = File(pickedFile!.path);
+    setState(() {
+      _image;
+      doImageLabeling();
+    });
+  }
+
+  //TODO choose image using gallery
+  _imgFromGallery() async {
+    XFile? pickedFile =
+    await imagePicker.pickImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
       setState(() {
-      _image =File(image.path);
+        _image = File(pickedFile.path);
+        doImageLabeling();
       });
     }
   }
-  capctureImage()async {
-    final XFile? image = await picker.pickImage(source: ImageSource.camera);
-    if(image !=null){
-      setState(() {
-      _image =File(image.path);
-      });
+
+  //TODO image labeling code here
+  doImageLabeling() async {
+    result = "";
+    final inputImage = InputImage.fromFile(_image!);
+    final List<ImageLabel> labels = await imageLabeler.processImage(inputImage);
+    for (ImageLabel label in labels) {
+      final String text = label.label;
+      final int index = label.index;
+      final double confidence = label.confidence;
+      result += "$text   ${confidence.toStringAsFixed(2)}\n";
     }
+    setState(() {
+      result;
+    });
   }
-  
+
   @override
   Widget build(BuildContext context) {
-    
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Image Picker'),
+      home: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('images/bg.jpg'), fit: BoxFit.cover),
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              _image !=null ? Image.file(_image!) : const Icon(Icons.image,size: 100,),
-      
-              ElevatedButton(
-                onPressed: () {
-                  chooseImage();
-                },
-                onLongPress: (){
-                  capctureImage();
-                },
-                child: const Text('Click to Capcture'),
-              )
-            ],
+        child: Scaffold(
+          body:  SingleChildScrollView(
+            child: Column(
+              children: [
+                const SizedBox(
+                  width: 100,
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 100),
+                  child: Stack(children: <Widget>[
+                    Stack(children: <Widget>[
+                      Center(
+                        child: Image.asset(
+                          'images/frame.png',
+                          height: 510,
+                          width: 500,
+                        ),
+                      ),
+                    ]),
+                    Center(
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            primary: Colors.transparent,
+                            shadowColor: Colors.transparent),
+                        onPressed: _imgFromGallery,
+                        onLongPress: _imgFromCamera,
+                        child: Container(
+                          margin: const EdgeInsets.only(top: 8),
+                          child: _image != null
+                              ? Image.file(
+                            _image!,
+                            width: 335,
+                            height: 495,
+                            fit: BoxFit.fill,
+                          )
+                              : Container(
+                            width: 340,
+                            height: 330,
+                            child: const Icon(
+                              Icons.camera_alt,
+                              color: Colors.black,
+                              size: 100,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ]),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 20),
+                  child: Text(
+                    result,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                        fontSize: 25, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ],
+            ),
           ),
+          backgroundColor: Colors.transparent,
         ),
       ),
     );
